3608488516fc59006608b0c2dce1f04b9dbd24af
fhict <fhict@MacBook.local>
2015-03-05 10:05:48 +0100

    Branch commit

---
+++ Hellglow World/Hellglow World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hellglow World
+//
+//  Created by fhict on 05/03/15.
+//  Copyright (c) 2015 fhict. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
